d137026fc2d2979cd356e6683ea235c42dd749f1
Fahad Kassim <educatedfahad@gmail.com>
2016-10-19 17:27:03 +0300

    The Actual Favicon and the Home Page

---
+++ index.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- Basic Page Needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Purple Cow Media</title>
+	<meta name="description" content="The Leading Full Fledge Advertising Agency in Tanzania">
+	<meta name="author" content="Nadhir M Bahayan">
+    <meta name="keywords" content="Full Fledge Advertising Agency, Advertise, Advertising, Advertising Agency, Media Agency, Creative, Graphic Design, Video Production, Audio Production, Research, Media Planning, Social Media, Marketing, Outdoor, SEO, Website Development, Website Design, Web, Design, App Development, Mobile App Development, Mobile App, Mobile App Design, UI, Software, Software Development, Software Design, Programmers, Designers, Client Servicing, Coolest, Craziest">
+    <meta name='application-name' content="Purple Cow Media Limited Website" >
+
+   <!-- Mobile Specific Metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+    ================================================== -->
+   <link rel="stylesheet" href="css/default.css">
+	<link rel="stylesheet" href="css/layout.css">
+   <link rel="stylesheet" href="css/media-queries.css">    
+
+   <!-- Script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="favicon.png" >
+
+</head>
+
+<body>
+    <div id='purple_background'></div>
+	<div id="preloader">      
+      <div id="status">
+         <img src="images/preloader.gif" height="128" width="128" alt="">
+      </div>
+   </div>
+
+   <!-- Intro Section
+   ================================================== -->
+   <section id="intro">
+       <div id='black_background'></div>
+   	<header class="row">	 
+
+			<div id="logo" >
+				<a href="#" >
+                 <img src="images/logo.png" alt="Zoon">                 
+                </a>
+			</div>
+
+		   <nav id="nav-wrap">
+
+		      <a class="menu-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+			   <a class="menu-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+		      <ul id="nav" class="nav">
+		         <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
+		         <li><a class="smoothscroll" href="#about">About</a></li>			         
+			      <li><a class="smoothscroll" href="#location">Location</a></li>
+			      <li><a class="smoothscroll" href="commission.html">Commission</a></li>
+		      </ul> <!-- end #nav -->
+
+		   </nav> <!-- end #nav-wrap --> 	        
+
+   	</header> <!-- Header End --> 
+    <p id='msg_waiting_txt'>Message will be back in <span id='msg_id'></span></p>
+   	<div  id="main" class="row">
+
+	   	<div class="twelve columns">
+                <h1>We are currently working on something awesome. <br /><span class='text-mediumpurple'>MOoo!</span></h1>
+
+                <p>WE are a design-obsessed, technology-savvy team of web gurus and brand honchos, dedicated to creating outstanding branded solutions to real-world business problems.</p>
+
+                <h5>Time Left Until Launching</h5>
+	   		<div id="counter" class="cf">
+	   			<span>134 <em>days</em></span> 
+ 					<span>12 <em>hours</em></span>
+					<span>50 <em>minutes</em></span>
+ 					<span>33 <em>seconds</em></span> 
+   			</div>
+            <p id='live_status'></p>
+            <a href='#' id='hide_msg_btn'>Hide Message</a>
+   			<!-- Begin Signup Form -->
+	         <div id="mc_embed_signup">
+	            <form action="email_subscribe.php" method="post" id="email_form" name="email_form" class="validate" novalidate>
+	                  
+	               <input type="email" value="" name="email" class="email" id="email" placeholder="Email Address to be informed" required>
+	               
+	               <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	               <div style="position: absolute; left: -5000px;"><input type="text" name="b_cdb7b577e41181934ed6a6a44_e65110b38d" value=""></div>
+	               
+	               <!-- <div class="clear"> --><input type="submit" value="Subscribe" name="subscribe" id="subscribe_btn" class="button"><!-- </div> -->
+	               
+	          	</form>
+	         </div>
+
+	         <ul class="social">
+	            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+	            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+	            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+	            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+	            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+	            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+	            <li><a href="#"><i class="fa fa-skype"></i></a></li>
+            </ul>
+
+         </div> 
+
+      </div> <!-- main end -->    	
+   </section> <!-- end intro section -->
+
+   <!-- About Section
+   ================================================== -->
+   <section id="about">
+
+      <div class="row section-header">
+
+      	<div class="twelve columns">	
+
+      		<div class="icon-wrap">
+            	<i class="fa fa-group"></i>
+         	</div>
+
+	         <h1>About Us.</h1>
+
+	         <p class="lead">
+                <br />
+                <br />
+                </p>
+
+         </div>
+
+      </div> <!-- end section-header -->             	
+
+      <div class="row section-content">
+				
+			<div class="six columns">
+		      <h3>Our Process.</h3>
+
+		      <p>Our work is built around a close-knit relationship with our clients: Understanding, listening &amp; asking questions are as important to us as designing, planning &amp; coding beautiful websites and apps.</p>
+	      </div>
+
+	      <div class="six columns">
+		      <h3>Our Approach.</h3>
+
+		      <p>We believe that good design is good for business , and that design has a major impact on the way users &amp; customers perceive, and respond to, the brands they interact with daily.</p>
+	      </div>            
+
+      </div> <!-- end section-content -->  
+
+      <div class="row section-content">
+				
+			<div class="six columns">
+		      <h3>Our Vision.</h3>
+                <p>To help brands express themselves via creative solutions.</p>
+	      </div>
+
+	      <div class="six columns">
+		      <h3>Our Objective.</h3>
+
+		      <p>To be an integrated marketing communications firm that provides brands with strategic marketing insights, strategic marketing planning and strategic creative solutions. </p>
+	      </div>  
+		  <div class='twelve columns'>
+			  <h3>Our Services</h3>
+		  </div>
+		  <div class='three columns'>
+			  Creative<br /> 
+			  Animation<br />
+			  Video Production<br />
+			  Audio Production<br />
+			  Research
+		  </div>
+		  <div class='three columns'>
+			  Photography<br />
+			  Brand Management<br />
+			  Public Relation<br />
+			  Training<br />
+			  Event Management
+		  </div>
+		  <div class='three columns'>
+			  Consultancy<br />
+			  Advertising<br />
+			  Software Development<br />
+			  App Development<br />
+			  Content
+		  </div>
+		  <div class='three columns'>
+			  Website Development<br />
+			  Print Production<br />
+			  SEO<br />
+			  Social Media Marketing<br />
+			  Editorial Services
+		  </div>
+
+      </div> <!-- end section-content -->       
+
+      <div id="call-to-action">	       
+
+		   <div class="row section-ads">
+
+		      <div class="twelve columns">		         		
+
+			      <h2>Let's Talk</h2>
+
+			      <p class="lead"> Our Website is under construction but our offices are open for business.<br /> Our Team is waiting for You.</p>
+
+					<div class="action">
+			         <!--<a href="#" class="button">Contact Us</a>-->
+	         	</div>
+
+			   </div>
+
+		   </div> <!-- end section-ads -->		         	         
+
+	   </div> <!-- end call-to-action -->		   	
+
+   </section> <!-- About Section End-->    
+
+
+   <!-- Location Section
+   ================================================== -->
+	<section id="location">
+
+		<div class="contacts">		
+			
+			<div class="row contact-details">		           		         
+
+			   <div class="columns">
+
+				   <h3><i class="fa fa-home"></i>Address.</h3>
+				   <p>Quality Centre Mall<br>
+						Pugu Rad, Dar es salaam <br>
+						Tanzania
+					</p>
+
+			   </div> 
+
+			   <div class="columns">
+
+				   <h3><i class="fa fa-phone"></i>Phone Numbers.</h3>
+				   <p>(+255) 689 886 999
+				  
+                                   <br> (+255) 788 228 836</p>
+
+			   </div>	 
+
+			   <div class="columns end">
+
+				   <h3><i class="fa fa-envelope"></i>Emails.</h3>
+				   <p>info@gopurplecow.com<br>
+				   </p>
+
+			   </div>          	
+
+		 	</div> <!-- end contact-details -->		  
+
+		</div> <!-- end contacts -->
+
+	   <div id="map">
+
+	   	 <p class="map-error">Something went wrong... Unable to load map... Please try to enable javascript</p>   
+
+	   </div> <!-- end map -->
+
+	</section> <!-- end location section -->
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">            
+
+            <ul class="copyright">
+               <li>&copy; Copyright 2016 Purple Cow Media</li>         
+            </ul>
+
+         </div>          
+
+      </div>
+
+      <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#intro"><i class="icon-up-open"></i></a></div>
+
+   </footer> <!-- Footer End-->   
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+	<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+   <script src="js/gmaps.js"></script>
+   <script src="js/waypoints.js"></script>
+   <script src="js/jquery.countdown.js"></script>
+   <script src="js/jquery.placeholder.js"></script>
+   <script src="js/backstretch.js"></script>  
+   <script src="js/init.js"></script>
+
+</body>
+
+</html>
