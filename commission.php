661bc4935cafb394d0a596a872bde0e203c911fb
Fahad Kassim <educatedfahad@gmail.com>
2016-10-19 17:23:26 +0300

    Commission Page

---
+++ commission.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<!--[if lt IE 8 ]><html class="no-js ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="no-js ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 8)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+
+   <!--- Basic Page Needs
+   ================================================== -->
+   <meta charset="utf-8">
+	<title>Purple Cow Media</title>
+	<meta name="description" content="The Leading Full Fledge Advertising Agency in Tanzania">
+	<meta name="author" content="Nadhir M Bahayan">
+    <meta name="keywords" content="Full Fledge Advertising Agency, Advertise, Advertising, Advertising Agency, Media Agency, Creative, Graphic Design, Video Production, Audio Production, Research, Media Planning, Social Media, Marketing, Outdoor, SEO, Website Development, Website Design, Web, Design, App Development, Mobile App Development, Mobile App, Mobile App Design, UI, Software, Software Development, Software Design, Programmers, Designers, Client Servicing, Coolest, Craziest">
+    <meta name='application-name' content="Purple Cow Media Limited Website" >
+
+   <!-- Mobile Specific Metas
+   ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+    ================================================== -->
+   <link rel="stylesheet" href="css/default.css">
+	<link rel="stylesheet" href="css/layout.css">
+   <link rel="stylesheet" href="css/media-queries.css">
+
+   <!-- Script
+   ================================================== -->
+	<script src="js/modernizr.js"></script>
+
+   <!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="favicon.png" >
+
+</head>
+
+<body>
+    <div id='purple_background'></div>
+	<div id="preloader">
+      <div id="status">
+         <img src="images/preloader.gif" height="128" width="128" alt="">
+      </div>
+   </div>
+
+   <!-- Intro Section
+   ================================================== -->
+   <section id="intro">
+       <div id='black_background'></div>
+   	<header class="row">
+
+			<div id="logo" >
+				<a href="#" >
+                 <img src="images/logo.png" alt="Zoon">
+                </a>
+			</div>
+
+		   <nav id="nav-wrap">
+
+		      <a class="menu-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+			   <a class="menu-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+		      <ul id="nav" class="nav">
+		         <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
+		         <li><a class="smoothscroll" href="#about">About</a></li>
+			      <li><a class="smoothscroll" href="#location">Location</a></li>
+			      <li><a class="smoothscroll" href="commission.html">Commission</a></li>
+		      </ul> <!-- end #nav -->
+
+		   </nav> <!-- end #nav-wrap -->
+
+   	</header> <!-- Header End -->
+    <p id='msg_waiting_txt'>Message will be back in <span id='msg_id'></span></p>
+   	<div  id="main" class="row">
+
+	   	<div class="twelve columns">
+			<h1>Are you looking for extra cash?<br /><span class='text-mediumpurple'>MOoo!</span></h1>
+
+			<p>We are looking for Commission Agents who will be working with us by bringing in business and in return we will provide:<br /><br /> - Daily Allowance <br /><br /> - Upto 5% Commission on each business brought in even before customer pays <br /><br /> - Once Achieved our Targets we will also provide monthly salaries</p>
+			<hr />
+			<h1>FILL IN THE FORM BELOW</h1>
+			<h5><a href='#' id='hide_msg_btn'>Click to Show Picture</a></h5>
+        </div>
+
+      </div> <!-- main end -->
+   </section> <!-- end intro section -->
+
+   <!-- About Section
+   ================================================== -->
+   <section id="about">
+
+      <div class="row section-header">
+
+      	<div class="twelve columns">
+	         <h1>Commission Agent Application</h1>
+			<p>Please fill in the field to get started.</p>
+         </div>
+
+      </div> <!-- end section-header -->
+	   <div id ='commission_form' class='row'>
+		   <h5>PERSONAL INFORMATION</h5>
+		   <span class='input-group'>
+			   <label for='first_name'>First Name</label>
+			   <input type='text' value='' name='first_name' placeholder='John'/>
+		   </span>
+		   <span class='input-group'>
+			   <label for='last_name'>Last Name</label>
+			   <input type='text' value='' name='last_name' placeholder="Doe" />
+		   </span>
+
+		   <h5>CONTACT DETAILS</h5>
+		   <span class='input-group'>
+			   <label for='mobile1'>Mobile 1</label>
+			   <input type='text' value='' name='mobile1' class='phone'/>
+		   </span>
+		   <span class='input-group'>
+			   <label for='mobile2'>Mobile 2</label>
+			   <input type='text' value='' name='mobile2' class='phone'/>
+		   </span>
+		   <span class='input-group'>
+			   <label for='mobile3'>Mobile 3</label>
+			   <input type='text' value='' name='mobile3' class='phone'/>
+		   </span>
+		   <br />
+		   <span class='input-group'>
+			   <label for='email1'>Email 1</label>
+			   <input type='text' value='' name='email1' placeholder="email@domain.com" size='40'/>
+		   </span>
+		   <span class='input-group'>
+			   <label for='email2'>Email 2</label>
+			   <input type='text' value='' name='email2' placeholder="email@domain.com" size='40'/>
+		   </span>
+
+		   <h5>Employment Information</h5>
+		   <span class='input-group'>
+			   <label for='employed'>Are you Employed or Self-Employed?</label>
+			   Employed: <input type='radio' name='employed' value='1'/>
+			   Self-Employed: <input type='radio' name='employed' value='0'/>
+		   </span>
+		   <br />
+		   <span class='input-group'>
+			   <label for='company'>Company</label>
+			   <input type='text' value='' name='company' placeholder="Company Limited"/>
+		   </span>
+
+		   <h5>Address</h5>
+		   <span class='input-group'>
+			   <label for='region'>Region</label>
+			   <input type='text' value='' name='region' placeholder="Dar es salaam"/>
+		   </span>
+		   <span class='input-group'>
+			   <label for='municipality'>Municipality</label>
+			   <input type='text' value='' name='municipality' placeholder="Kinondoni"/>
+		   </span>
+		   <span class='input-group'>
+			   <label for='street'>Street</label>
+			   <input type='text' value='' name='street' placeholder="Libya Street"/>
+		   </span>
+		   <span class='input-group'>
+			   <label for='house'>House Number</label>
+			   <input type='text' value='' name='house' placeholder="23" />
+		   </span>
+
+		   <h5>More Information</h5>
+		   <div class="four columns">
+			   <p><input type='checkbox' name='fields' value='creative' /> Creative &amp; Design</p>
+			   <p><input type='checkbox' name='fields' value='print' /> Print Production</p>
+			   <p><input type='checkbox' name='fields' value='photography' /> Photography</p>
+			   <p><input type='checkbox' name='fields' value='video' /> Video Production</p>
+			   <p><input type='checkbox' name='fields' value='audio' /> Audio Produciton</p>
+		   </div>
+		   <div class="four columns">
+			   <p><input type='checkbox' name='fields' value='smm' /> Social Media Marketing</p>
+			   <p><input type='checkbox' name='fields' value='seo' /> SEO</p>
+			   <p><input type='checkbox' name='fields' value='website' /> Website Development</p>
+			   <p><input type='checkbox' name='fields' value='mobile' /> Mobile App Development</p>
+			   <p><input type='checkbox' name='fields' value='software' /> Software Development</p>
+		   </div>
+		   <div class="four columns">
+			   <p><input type='checkbox' name='fields' value='event' /> Event Management</p>
+			   <p><input type='checkbox' name='fields' value='animation' /> Animation</p>
+			   <p><input type='checkbox' name='fields' value='public' /> Public Relations</p>
+			   <p><input type='checkbox' name='fields' value='research' /> Research</p>
+			   <p><input type='checkbox' name='fields' value='content' /> Content &amp; Editorial</p>
+		   </div>
+
+		   <p>All information provided in this form will not be shared or sold or used in any other work apart from the one intended at Purple Cow Media Ltd. </p>
+		   <div id='error_msg'></div>
+		   <a href="#" class="button" id='commission_btn'>Submit Form</a>
+	   </div><!-- commission_form -->
+
+      <div id="call-to-action">
+
+		   <div class="row section-ads">
+
+		      <div class="twelve columns">
+
+			      <h2>Let's Talk</h2>
+
+			      <p class="lead"> Our Website is under construction but our offices are open for business.<br /> Our Team is waiting for You.</p>
+
+					<div class="action">
+			         <!--<a href="#" class="button">Contact Us</a>-->
+	         	</div>
+
+			   </div>
+
+		   </div> <!-- end section-ads -->
+
+	   </div> <!-- end call-to-action -->
+
+   </section> <!-- About Section End-->
+
+
+   <!-- Location Section
+   ================================================== -->
+	<section id="location">
+
+		<div class="contacts">
+
+			<div class="row contact-details">
+
+			   <div class="columns">
+
+				   <h3><i class="fa fa-home"></i>Address.</h3>
+				   <p>Quality Centre Mall<br>
+						Pugu Rad, Dar es salaam <br>
+						Tanzania
+					</p>
+
+			   </div>
+
+			   <div class="columns">
+
+				   <h3><i class="fa fa-phone"></i>Phone Numbers.</h3>
+				   <p>Phone: (255) 787 487 333<br>
+				   </p>
+
+			   </div>
+
+			   <div class="columns end">
+
+				   <h3><i class="fa fa-envelope"></i>Emails.</h3>
+				   <p>info@gopurplecow.com<br>
+				   </p>
+
+			   </div>
+
+		 	</div> <!-- end contact-details -->
+
+		</div> <!-- end contacts -->
+
+	   <div id="map">
+
+	   	 <p class="map-error">Something went wrong... Unable to load map... Please try to enable javascript</p>
+
+	   </div> <!-- end map -->
+
+	</section> <!-- end location section -->
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <ul class="copyright">
+               <li>&copy; Copyright 2016 Purple Cow Media</li>
+            </ul>
+
+         </div>
+
+      </div>
+
+      <div id="go-top"><a class="smoothscroll" title="Back to Top" href="#intro"><i class="icon-up-open"></i></a></div>
+
+   </footer> <!-- Footer End-->
+
+   <!-- Java Script
+   ================================================== -->
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+   <script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+   <script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+
+	<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
+   <script src="js/gmaps.js"></script>
+   <script src="js/waypoints.js"></script>
+   <script src="js/jquery.countdown.js"></script>
+   <script src="js/jquery.placeholder.js"></script>
+   <script src="js/backstretch.js"></script>
+   <script src='js/phone-format.js'></script>
+   <script src="js/init.js"></script>
+
+
+</body>
+
+</html>
